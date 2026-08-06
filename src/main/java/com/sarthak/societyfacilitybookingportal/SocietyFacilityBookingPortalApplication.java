b8de0b623dabc9d638bf39dc8ca76e824eea86d9
Sarthak Chaudhari <codeSarthak02@gmail.com>
2026-08-06 11:03:27 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/sarthak/societyfacilitybookingportal/SocietyFacilityBookingPortalApplication.java
@@ -0,0 +1,13 @@
+package com.sarthak.societyfacilitybookingportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocietyFacilityBookingPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocietyFacilityBookingPortalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sarthak/societyfacilitybookingportal/SocietyFacilityBookingPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarthak.societyfacilitybookingportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocietyFacilityBookingPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
